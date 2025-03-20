5259346b17536000b29e23f003b6afd187e9c386
Jakababa94 <jakababa002@gmail.com>
2025-03-20 17:57:38 +0300

    Initial commit

---
+++ assignment.py
@@ -0,0 +1,40 @@
+#creating an empty list
+
+my_list =[]
+print(my_list)
+
+#appending elements to the list
+my_list = [10,20,30,40]
+print(my_list)
+
+#inserting elements to the list
+my_list = [10,20,30,40]
+my_list.insert(1,15)
+print(my_list)
+
+#extending the list
+my_list = [10,20,30,40]
+my_list1 = [50,60,70]
+my_list.extend(my_list1)
+print(my_list)
+
+#removing elements from the list
+my_list = [10,20,30,40]
+my_list.remove(40)
+print(my_list)
+
+
+#my_list in ascending order
+my_list = [10,20,30,40]
+my_list.sort()
+print(my_list)
+
+#index of an element(30) in the list
+my_list = [10,20,30,40]
+print(my_list.index(30))
+
+
+
+
+
+
